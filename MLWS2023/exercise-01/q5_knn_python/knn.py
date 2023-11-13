565e9be36a782ea35298dc41d6598f62d27c31ba
pawanchand <chandpawan0@gmail.com>
2023-11-13 15:21:52 +0100

    added k nearest neighbor density estimation code in knn.py

--- MLWS2023/exercise-01/q5_knn_python/knn.py
+++ MLWS2023/exercise-01/q5_knn_python/knn.py
@@ -11,4 +11,14 @@ def knn(samples, k):
 
     #####Insert your code here for subtask 5b#####
     # Compute the number of the samples created
+    N = len(samples)
+    pos =  np.arange(-5,5.0,0.1).reshape(-1,1)
+    prob = np.zeros(pos.shape)
+
+    dist = np.abs((pos-samples.reshape(-1,N))) #100xN
+    sorted_dist = np.argsort(dist, axis = -1)   #100xN
+    v= dist[np.where(sorted_dist ==k-1)].reshape(-1,1)       #100x1
+    prob = k/(2*N*v).reshape(-1,1)            #100x1
+    estDensity  = np.hstack((pos,prob))
+
     return estDensity
