d7bc3310ba5e250dd8b5d91a24b0e5338beb05b4
pawanchand <chandpawan0@gmail.com>
2023-11-13 16:29:24 +0100

    added code in the file getLogLikelihood.py

--- MLWS2023/exercise-01/q6_expectation_maximization_python/getLogLikelihood.py
+++ MLWS2023/exercise-01/q6_expectation_maximization_python/getLogLikelihood.py
@@ -15,5 +15,18 @@ def getLogLikelihood(means, weights, covariances, X):
     # logLikelihood  : log-likelihood
 
     #####Insert your code here for subtask 6a#####
+
+    K,D = np.array(means).shape
+    N = X.shape[0]
+    
+    logLikelihood=0
+    for n in range(N):
+        weighted_gauss = 0
+        for i in range(K):
+            exponent = (X[n]-means[i])@np.linalg.inv(covariances[:,:,i])@(X[n]-means[i]).T
+            base = (((2*np.pi)**(D/2))*np.sqrt(np.linalg.det(covariances[:,:,i])))
+            weight = np.array(weights).reshape((1,-1))[0,i]
+            weighted_gauss+= weight*np.exp(-0.5*exponent)/base
+        logLikelihood+= np.log(weighted_gauss)
     return logLikelihood
 
