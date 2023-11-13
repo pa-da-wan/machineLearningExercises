7e0864482ddc507dc76210bafe74b9eb735b0f28
pawanchand <chandpawan0@gmail.com>
2023-11-13 18:04:50 +0100

    added code to EStep.py

--- MLWS2023/exercise-01/q6_expectation_maximization_python/EStep.py
+++ MLWS2023/exercise-01/q6_expectation_maximization_python/EStep.py
@@ -20,4 +20,20 @@ def EStep(means, covariances, weights, X):
     # gamma          : NxK matrix of responsibilities for N datapoints and K Gaussians.
 
     #####Insert your code here for subtask 6b#####
+    logLikelihood = getLogLikelihood(means=means, weights=weights, covariances=covariances, X=X)
+    K, D = np.array(means).shape
+    N = X.shape[0]
+
+    gamma = np.zeros((N, K))
+
+    for i in range(K):
+        diff = X - means[i]
+        inv_cov = np.linalg.pinv(covariances[:, :, i])
+        weight = np.array(weights).reshape((1,-1))[0,i]
+        num = weight * np.exp(-0.5 * np.sum(diff @ inv_cov * diff, axis=1))
+        denom = np.sqrt(np.linalg.det(covariances[:, :, i]) * (2 * np.pi) ** D)
+        gamma[:, i] = num / denom
+
+    # Normalize gamma along axis 1
+    gamma /= gamma.sum(axis=1, keepdims=True)
     return [logLikelihood, gamma]
