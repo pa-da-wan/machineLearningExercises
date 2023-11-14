ae77c979d7ea45b90b6043ec926fb9701ad7d919
pawanchand <chandpawan0@gmail.com>
2023-11-14 18:02:31 +0100

    added exercise_02 problems

---
+++ MLWS2023/exercise_02/q1_leastSquare_linear_classifier_python/leastSquares.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def leastSquares(data, label):
+    # Sum of squared error shoud be minimized
+    #
+    # INPUT:
+    # data        : Training inputs  (num_samples x dim)
+    # label       : Training targets (num_samples x 1)
+    #
+    # OUTPUT:
+    # weights     : weights   (dim x 1)
+    # bias        : bias term (scalar)
+
+    #####Insert your code here for subtask 1a#####
+    # Extend each datapoint x as [1, x]
+    # (Trick to avoid modeling the bias term explicitly)
+    return weight, bias
---
+++ MLWS2023/exercise_02/q1_leastSquare_linear_classifier_python/linclass.py
@@ -0,0 +1,16 @@
+def linclass(weight, bias, data):
+    # Linear Classifier
+    #
+    # INPUT:
+    # weight      : weights                (dim x 1)
+    # bias        : bias term              (scalar)
+    # data        : Input to be classified (num_samples x dim)
+    #
+    # OUTPUT:
+    # class_pred       : Predicted class (+-1) values  (num_samples x 1)
+
+    #####Insert your code here for subtask 1b#####
+    # Perform linear classification i.e. class prediction
+    return class_pred
+
+
